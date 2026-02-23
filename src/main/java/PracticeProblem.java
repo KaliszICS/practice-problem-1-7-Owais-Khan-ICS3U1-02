4a0e9a7e45aad99c4370894ad9b3e3fbf5ae5d07
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-02-23 16:04:38 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,11 @@
+/**
+	* File: Lesson 1.7 - Booleans (and, or, not)
+	* Author: Owais Ali Khan
+	* Date Created: Feb 23, 2026
+	* Date Last Modified: Feb 23, 2026
+	*/
+
+import java.util.Scanner; 
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -10,27 +18,48 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		boolean bool1 = true;
+		boolean bool2 = false;
+		System.out.println(bool1 && bool2);
+		System.out.println(bool1 || bool2);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a lowercase letter: ");
+		char letter = input.next().charAt(0);
+		System.out.print("Input another lowercase letter: ");
+		char letter1 = input.next().charAt(0);
+		System.out.println(letter < letter1);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num = input.nextDouble();
+		System.out.println(num >= 0 && num <= 10 );
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num = input.nextDouble();
+		System.out.println(num > 0 && num != 5);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a word: ");
+		String word = input.nextLine();
+		System.out.println(!word.equals("banana"));
+
 	}
 
 	public static void q6() {
-		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print( "Enter an integer: ");
+		int num = input.nextInt();
+		System.out.println("The integer " + num + " is " + (num%2==0));
 	}
 
 }
